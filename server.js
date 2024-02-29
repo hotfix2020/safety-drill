33f95611423f781580b564c41473f3a80c1483ae
wanner <ljzsyww@foxmial.com>
2024-02-29 08:59:00 +0800

    feat:修改csp配置

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ if (IS_SECURITY === 'open') {
 	app.use(helmet.frameguard({ action: 'deny' }))
 	// 设置Content-Security-Policy
 	app.use(
-		helmet.contentSecurityPolicy({
-			directives: {
-				defaultSrc: ["'self'"], // 只允许执行同源的脚本
-				scriptSrc: ["'self'", 'https://example.com'], // 允许执行这些源的脚本
-				styleSrc: ["'self'", "'unsafe-inline'", 'https://example.com'], // 允许使用自身和指定example上的样式表
-				objectSrc: ["'none'"], // 不允许<object>, <embed>, 和<applet>元素加载任何资源
-				// upgradeInsecureRequests: [], // 将不安全的请求（http）升级为安全的请求（https）
-				frameAncestors: ["'self'"], // 与X-Frame-Options的DENY相同，禁止页面被嵌入到任何iframe或frame中
+		helmet({
+			contentSecurityPolicy: {
+				directives: {
+					defaultSrc: ["'self'"], // 只允许执行同源的脚本
+					scriptSrc: ["'self'", 'https://example.com'], // 允许执行这些源的脚本
+					styleSrc: ["'self'", "'unsafe-inline'", 'https://example.com'], // 允许使用自身和指定example上的样式表
+					objectSrc: ["'none'"], // 不允许<object>, <embed>, 和<applet>元素加载任何资源
+					// upgradeInsecureRequests: [], // 将不安全的请求（http）升级为安全的请求（https）
+					frameAncestors: ["'self'"], // 与X-Frame-Options的DENY相同，禁止页面被嵌入到任何iframe或frame中
+				},
 			},
-			// 设置报告URI，可用于收集违反CSP的报告
-			// reportUri: '/csp-violation-report-endpoint'
 		})
 	)
 }
@@ -45,7 +45,7 @@ app.use(cookieParser())
 
 // 自定义中间件来设置cookie
 app.use((req, res, next) => {
-	res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+	res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
 	// 检查请求中是否已有cookie，如果没有则设置一个
 	if (!req.cookies.token) {
 		// 设置cookie，此处为示例，实际应用中可能没有cookie需要登录验证之类的
